ed93cac9c223f24ff4b79e154c718e3af5e9940c
Federico Calo <fedcal01@gmail.com>
2022-05-04 16:07:13 +0200

    updates

--- data_cleaner.py
+++ data_cleaner.py
@@ -8,7 +8,18 @@ def main():
     links_csv = pd.read_csv("./dataset/links_small.csv", sep=",")
     keywords_csv=pd.read_csv("./dataset/keywords.csv", sep=",")
     movies_metadata= pd.read_csv("./dataset/movies_metadata.csv", sep=",", low_memory=False)
+    ratings=pd.read_csv("./dataset/ratings.csv", sep=",")
+    ratings_small=pd.read_csv("./dataset/ratings_small.csv.csv",sep=",")
 
     #elimino le colonne da movies_metadata che non mi interessano
-    movies_metadata_clean = movies_metadata.drop(columns=['belongs_to_collection', 'budget', 'homepage', 'imdb_id', 'poster_path', 'production_companies','production_countries', 'revenue', 'status', 'video'])
-    movies_metadata_clean.to_csv('./dataset/movies_metadata_clean.csv', index=True)
+    movies_metadata_clean = movies_metadata.drop(columns=['belongs_to_collection', 'budget', 'homepage', 'imdb_id', 'poster_path', 'revenue', 'status', 'video'])
+    movies_metadata_clean.to_csv('./dataset/movies_metadata_clean.csv', index=False)
+
+    """#elimino le righe dal file keywords con campo vuoto
+    keywords_csv_clean = keywords_csv
+    rowindex = 0
+    for row in keywords_csv_clean['keywords']:
+        if row is not None and row == '[]':
+            keywords_csv_clean.drop(rowindex, axis=0, inplace=True)
+        rowindex += 1
+    keywords_csv_clean.to_csv("./dataset/keywords_clean.csv")"""
--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    movies_metadata= pd.read_csv("./dataset/movies_metadata.csv", sep=",", low_memory=False)
-    print(movies_metadata.columns)
+    movies_metadata_clean = pd.read_csv("./dataset/movies_metadata_clean.csv", sep=",", low_memory=False)
+    column_generes=movies_metadata_clean['genres'].tolist()
+
+    print(column_generes[0])
+    #dictionary=pd.DataFrame.from_records(column_generes[0])
 
 
+    df2=movies_metadata_clean.explode('genres').reset_index(drop=True)
+    df3=pd.DataFrame.from_records(df2['genres'].values)
+    df2=df2.join(df3).drop(columns=['genres'])
+    index=0
+    print(df2.loc[1])
 
-
-
-
-
+    """result = []
+    for idx, row in movies_metadata_clean.iterrows():
+        for dct in row['genres']:
+            dct['name'] = row['name']
+            result.append(dct)
+    result = pd.DataFrame(result)
+    result['rank'] = result['rank'].astype(np.int32)
+    result['price'] = result['price'].str.replace('$', '')
+    result['price'] = result['price'].astype('float')
+    print(result)"""
+    """for row in column_generes:
+        dict= pd.json_normalize(row)
+        print(dict)"""
